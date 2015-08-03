ca255492027fc12c060cab8061e5c67f13ca2914
NancyKo <konancys@gmail.com>
2015-08-03 16:08:39 -0400

    Four methods finished

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,13 @@ end
 def sort_array_desc(array)
   array.sort {|x,y| y<=>x}
 end
+
+def swap_elements(array)
+  array << array[1]
+  array.delete_at(1)
+  return array
+end
+
+def reverse_array(array)
+  array.reverse 
+end
