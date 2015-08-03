ef73aa11915d504e4d8dd32c14a7fe4da118c8d7
NancyKo <konancys@gmail.com>
2015-08-03 16:25:40 -0400

    Seven methods completed

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def sort_array_asc(array)
   array.sort!
 end
@@ -14,4 +15,19 @@ end
 
 def reverse_array(array)
   array.reverse 
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = '$'  
+  end
+end
+
+def find_a(array)
+  all_a = array.collect { |word| word if word[0] == "a" }
+  all_a.compact
+end
+
+def sum_array(array)
+  array.reduce(:+)
 end
